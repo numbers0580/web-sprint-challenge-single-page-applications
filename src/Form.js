de756d32b56d4e1bd035f1ee5bdc3aba836c647a
numbers0580 <koreankid18@yahoo.com>
2020-06-19 10:42:11 -0600

    Created forms. Now to make them functional.

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import Order from './Order';
 
 function Form (props) {
     return (
@@ -6,6 +8,27 @@ function Form (props) {
             <h2>Welcome to Lambda Eats: Where the food will make you say, "Oh Mega!"</h2>
             <p>This homepage form should include:<br />
             Name, Address, Phone Number, Pickup/Delivery option, Store Location dropdown</p>
+            <div class='mainFormDiv'>
+                <label>Your Name:&nbsp;</label>
+                <input type='text' />
+            </div>
+            <div class='mainFormDiv'>
+                <label>Address:&nbsp;</label>
+                <input type='text' />
+            </div>
+            <div class='mainFormDiv'>
+                <label>Phone Number:&nbsp;</label>
+                <input type='phone' />
+            </div>
+            <div class='mainFormDiv'>
+                <label>Pickup or Delivery:&nbsp;</label>
+                <input type='radio' name='puDelivery' />Pickup&nbsp;
+                <input type='radio' name='puDelivery' />Delivery
+            </div>
+            <BrowserRouter>
+                <Link to='/pizza'><button>Customize Pizza</button></Link>
+                <Route path='/pizza' component={Order} />
+            </BrowserRouter>
         </form>
     );
 }

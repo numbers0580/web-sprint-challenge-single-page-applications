7029d7faf520eb2d06a459290c51e5ee6ff54851
numbers0580 <koreankid18@yahoo.com>
2020-06-19 11:36:16 -0600

    Working on formSchema. Having difficulty enabling disabled button.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
 import React, { useState, useEffect } from "react";
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import * as yup from 'yup';
+import {v4} from 'uuid';
 import Form from './Form';
 import Order from './Order';
 
+const formSchema = yup.object().shape({
+  custName: yup
+    .string()
+    .min(2, "Your name must be at least 2 characters")
+    .required("A name is required to place the order")
+});
+
+const blankForm = {
+  custName: '',
+  custAddr: '',
+  custPhone: '',
+  puDelivery: ''
+};
+
+const blankErrors = {
+  custName: '',
+  custAddr: '',
+  custPhone: '',
+  puDelivery: ''
+};
+
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [mainEntries, updateEntries] = useState(blankForm);
+  const [mainErrors, updateErrors] = useState(blankErrors);
+  const [isDisabled, changeDisabled] = useState(true);
+
+  const changedMainForm = function(event) {
+    const {name, value} = event.target;
+
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(() => {
+        updateErrors({...mainErrors, [name]: ""});
+      })
+      .catch(err => {
+        updateErrors({...mainErrors, [name]: err.errors[0]});
+      })
+
+    updateEntries({...mainEntries, [name]: value});
+  };
+
+  const clickedCustomize = function(cEvent) {
+    cEvent.preventDefault();
+
+    const newCustomer = {
+      id: v4(),
+      custName: mainEntries.custName.trim(),
+      custAddr: mainEntries.custAddr.trim(),
+      custPhone: mainEntries.custPhone.trim(),
+      puDelivery: mainEntries.puDelivery
+    };
+  }
+
+  useEffect(() => {
+    formSchema.isValid(mainEntries).then(valid => {
+      changeDisabled(!valid);
+    })
+  }, [mainEntries]);
+
   return (
     <BrowserRouter>
       <header>
@@ -12,7 +74,7 @@ const App = () => {
           <div className="mainBtns">
             <Link to='/'><button>Home</button></Link>
             <button>Menu</button>
-            <Link to='/pizza'><button>Order</button></Link>
+            {/* <Link to='/pizza'><button>Order</button></Link> */}
           </div>
           <div className="rightBtn">
             <button>Sign In</button>
@@ -20,10 +82,10 @@ const App = () => {
         </nav>
       </header>
       <Switch>
+        <Route exact path='/pizza' component={Order} />
         <Route exact path='/'>
-          <Form />
+          <Form entries={mainEntries} inputChange={changedMainForm} inputErrors={mainErrors} disability={isDisabled} formNext={clickedCustomize} />
         </Route>
-        <Route path='/pizza' component={Order} />
       </Switch>
     </BrowserRouter>
   );
--- src/Form.js
+++ src/Form.js
@@ -3,30 +3,32 @@ import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import Order from './Order';
 
 function Form (props) {
+    const {entries, inputChange, inputErrors, disability, formNext} = props;
+
     return (
         <form>
-            <h2>Welcome to Lambda Eats: Where the food will make you say, "Oh Mega!"</h2>
+            <h2>Welcome to Lambda Eats: Where our food will make you say, "Oh Mega!"</h2>
             <p>This homepage form should include:<br />
             Name, Address, Phone Number, Pickup/Delivery option, Store Location dropdown</p>
             <div class='mainFormDiv'>
                 <label>Your Name:&nbsp;</label>
-                <input type='text' />
+                <input type='text' name='custName' />
             </div>
             <div class='mainFormDiv'>
                 <label>Address:&nbsp;</label>
-                <input type='text' />
+                <input type='text' name='custAddr' />
             </div>
             <div class='mainFormDiv'>
                 <label>Phone Number:&nbsp;</label>
-                <input type='phone' />
+                <input type='phone' name='custPhone' />
             </div>
             <div class='mainFormDiv'>
                 <label>Pickup or Delivery:&nbsp;</label>
-                <input type='radio' name='puDelivery' />Pickup&nbsp;
-                <input type='radio' name='puDelivery' />Delivery
+                <input type='radio' name='puDelivery' value='Pickup' />Pickup&nbsp;
+                <input type='radio' name='puDelivery' value='Delivery' />Delivery
             </div>
             <BrowserRouter>
-                <Link to='/pizza'><button>Customize Pizza</button></Link>
+                <Link to='/pizza'><button type='submit' name='submitBtn' disabled={disability}>Customize Pizza</button></Link>
                 <Route path='/pizza' component={Order} />
             </BrowserRouter>
         </form>
